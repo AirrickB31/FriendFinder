25a1f6019cea3949eeb61e92c54b4a052f9509e7
ebartos <eric@frequency.com>
2018-05-27 23:49:48 -0700

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+const friendsData =[{
+
+    name: 'Super Man',
+    photo: 'https://tinyurl.com/y9sjwkpu', 
+    scores: [2,3,4,5,2,3,3,4,3,3]
+},{
+    name: 'Big Momma',
+    photo: 'https://tinyurl.com/ybzx8q9r', 
+    scores: [2,3,1,1,2,3,4,2,3,3]
+},{
+    name: 'Jay and Slient Bob',
+    photo: 'https://tinyurl.com/ya72l6xl', 
+    scores: [5,3,1,1,5,3,5,2,3,3]
+}
+];
+
+module.exports = friendsData;
---
+++ server.js
@@ -0,0 +1,21 @@
+const  express = require("express"); 
+const path = require("path"); 
+const bodyParser = require("body-parser"); 
+
+const app = express(); 
+
+const PORT = process.env.PORT || 8080;
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.json())
+
+
+require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
+
+app.listen(PORT, function(){
+    console.log('App listenting on PORT: ' + PORT);
+});
+
